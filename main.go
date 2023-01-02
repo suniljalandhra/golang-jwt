4c58916cd71869870da284f69de72c03116376df
Sunil Jalandhra <33174834+suniljalandhra@users.noreply.github.com>
2023-01-02 22:06:24 +0530

    feat: add api and routes

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/suniljalandhra/golang-jwt-project/routes"
 )
 
 func main() {
@@ -15,4 +16,17 @@ func main() {
 
 	router := gin.New()
 	router.Use(gin.Logger())
+
+	routes.AuthRoutes(router)
+	routes.UserRoutes(router)
+
+	router.GET("/api-1", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{"success": "access granted for api-1"})
+	})
+
+	router.GET("/api-2", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{"success": "access granted for api-2"})
+	})
+
+	router.Run(":" + port)
 }
