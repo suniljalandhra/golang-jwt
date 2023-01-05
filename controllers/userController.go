6732edf79ee2c1dd1407a07b7274a3cec5866cca
Sunil Jalandhra <33174834+suniljalandhra@users.noreply.github.com>
2023-01-05 11:50:02 +0530

    feat : add the get user function in jwt

--- controllers/userController.go
+++ controllers/userController.go
@@ -1,8 +1,16 @@
 package controllers
 
 import (
+	"context"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
 	"github.com/suniljalandhra/golang-jwt-project/database"
+	helper "github.com/suniljalandhra/golang-jwt-project/helpers"
+	"github.com/suniljalandhra/golang-jwt-project/models"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -19,4 +27,23 @@ func Login()
 
 func GetUsers()
 
-func GetUser()
+// Only the admin can excess this function
+func GetUser() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		userId := c.Param("user_id")
+
+		if err := helper.MatchUserTypeToUid(c, userId); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+
+		var user models.User
+		err := userCollection.FindOne(ctx, bson.M{"user_id": userId}).Decode(&user)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		c.JSON(http.StatusOK, user)
+	}
+}
